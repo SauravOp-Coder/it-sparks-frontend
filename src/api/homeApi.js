737aa38e8db78a66f416db1e8cd90f57c41942a3
SauravOp-Coder <sauravshinde992@gmail.com>
2026-08-07 00:48:28 +0530

    c

--- src/api/homeApi.js
+++ src/api/homeApi.js
@@ -2,12 +2,12 @@
 import API from "./axiosConfig";
 
 export const getHomeContentApi = async () => {
-  const response = await API.get("/home");
+  const response = await API.get("/Home");
   return response.data;
 };
 
 export const updateHomeContentApi = async (formData) => {
-  const response = await API.put("/home", formData, {
+  const response = await API.put("/Home", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
