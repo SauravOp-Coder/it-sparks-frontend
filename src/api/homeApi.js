5c979cf0e4d7f94c95c28e9799268a271f61242a
SauravOp-Coder <sauravshinde992@gmail.com>
2026-08-06 23:12:48 +0530

    v

--- src/api/homeApi.js
+++ src/api/homeApi.js
@@ -1,16 +1,19 @@
-import API from "./axiosConfig";
+import axiosInstance from "./axiosInstance"; // or your axios config
 
 export const getHomeContentApi = async () => {
-  const response = await API.get("/home");
+  const response = await axiosInstance.get("/api/home");
   return response.data;
 };
 
-export const updateHomeContentApi = async (formData) => {
-  const response = await API.put("/home", formData, {
+export const updateHomeContentApi = async (data) => {
+  // If sending FormData (files), let Axios set boundary automatically.
+  // If sending clean JSON, send standard application/json headers.
+  const isFormData = data instanceof FormData;
+
+  const response = await axiosInstance.put("/api/home", data, {
     headers: {
-      "Content-Type": "multipart/form-data",
+      "Content-Type": isFormData ? "multipart/form-data" : "application/json",
     },
   });
-
   return response.data;
 };
