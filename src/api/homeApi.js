5b4b895b2d62824e2a30705c49fccc7a128d095d
SauravOp-Coder <sauravshinde992@gmail.com>
2026-08-07 10:24:33 +0530

    d

--- src/api/homeApi.js
+++ src/api/homeApi.js
@@ -1,12 +1,12 @@
 import API from "./axiosConfig.js";
 
-// Export named function expected by HomeContentBuilder.jsx
+// Fetch home page content
 export const getHomeContentApi = async () => {
   const response = await API.get("/home");
   return response.data;
 };
 
-// Export update function
+// Update home page content
 export const updateHomeContentApi = async (data) => {
   const response = await API.put("/home", data);
   return response.data;
