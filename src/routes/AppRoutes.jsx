c69d08572c339fdfa4c725f02d75db26d382273a
SauravOp-Coder <sauravshinde992@gmail.com>
2026-08-07 13:37:29 +0530

    c

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import ScrollToTop from "../components/ScrollToTop";
 import PublicLayout from "../layouts/PublicLayout";
 import AdminLayout from "../layouts/AdminLayout";
 
@@ -28,39 +28,57 @@ import ManageSettings from "../pages/admin/ManageSettings";
 
 import ManageBanners from "../pages/admin/ManageBanners";
 
-const AppRoutes = () => {
+
+  const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<PublicLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/courses/:id" element={<CourseDetail />} />
-        <Route path="/placements" element={<Placements />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetail />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-      </Route>
+    <>
 
-      <Route path="/admin/login" element={<AdminLogin />} />
+      <ScrollToTop />
 
-      <Route element={<AdminProtectedRoute />}>
-  <Route element={<AdminLayout />}>
-    <Route path="/admin/dashboard" element={<AdminDashboard />} />
-    <Route path="/admin/manage-home" element={<ManageHome />} />
-    <Route path="/admin/courses" element={<ManageCourses />} />
-    <Route path="/admin/blogs" element={<ManageBlogs />} />
-    <Route path="/admin/reviews" element={<ManageReviews />} />
-    <Route path="/admin/banners" element={<ManageBanners />} />
-    <Route path="/admin/placements" element={<ManagePlacements />} />
-    <Route path="/admin/gallery" element={<ManageGallery />} />
-    <Route path="/admin/enquiries" element={<ViewEnquiries />} />
-    <Route path="/admin/settings" element={<ManageSettings />} />
-  </Route>
-</Route>
-    </Routes>
+      <Routes>
+
+        <Route element={<PublicLayout />}>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:id" element={<CourseDetail />} />
+          <Route path="/placements" element={<Placements />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+
+        </Route>
+
+
+        <Route path="/admin/login" element={<AdminLogin />} />
+
+
+        <Route element={<AdminProtectedRoute />}>
+
+          <Route element={<AdminLayout />}>
+
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            <Route path="/admin/manage-home" element={<ManageHome />} />
+            <Route path="/admin/courses" element={<ManageCourses />} />
+            <Route path="/admin/blogs" element={<ManageBlogs />} />
+            <Route path="/admin/reviews" element={<ManageReviews />} />
+            <Route path="/admin/banners" element={<ManageBanners />} />
+            <Route path="/admin/placements" element={<ManagePlacements />} />
+            <Route path="/admin/gallery" element={<ManageGallery />} />
+            <Route path="/admin/enquiries" element={<ViewEnquiries />} />
+            <Route path="/admin/settings" element={<ManageSettings />} />
+
+          </Route>
+
+        </Route>
+
+
+      </Routes>
+
+    </>
   );
 };
 
